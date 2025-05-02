8df4b4f2936672df818c3b8dba5892dde125749d
Lorraine Warren <lorrywarren@gmail.com>
2025-05-02 16:35:22 +0100

    Add the logic for the available options along with error handling to prevent any unsupported operations from breaking the program.

--- Console_ToDoList/Program.cs
+++ Console_ToDoList/Program.cs
@@ -15,7 +15,7 @@ namespace Console_ToDoList
 
             // Create a list to hold the task items
             List<string> taskList = new List<string>();
-            // create user option varialbe, default to empty string
+            // create user option variable, default to empty string
             string option = "";
 
             while (option != "e" || option != "E")
@@ -26,6 +26,64 @@ namespace Console_ToDoList
                 Console.WriteLine("\n Enter 3 to view the whole list");
                 Console.WriteLine("\n Enter e to exit the program :) \n");
             }
+            // Read the users inputted value
+            option = Console.ReadLine();
+
+            // Add item to list
+            if (option == "1")
+            {
+                // Ask user to enter a new task name
+                Console.WriteLine("Enter new task name");
+                // Read the response
+                string newTask = Console.ReadLine();
+                // Add it to the list
+                taskList.Add(newTask);
+                Console.WriteLine("Task added to the list");
+            }
+            // Delete an item from the list
+            else if (option == "2")
+            {
+                // Iterate through the list 
+                for (int i = 0; i <taskList.Count; i++)
+                {
+                    Console.WriteLine(i + ":" + taskList[i]);
+                }
+                // Tell user to input a task number to be removed
+                Console.WriteLine("Enter the task number you'd like to remove");
+                // Convert string into int
+                int taskNumber = Convert.ToInt32(Console.ReadLine());
+            }
+            // View of tasks in the list
+            else if (option == "3")
+            {
+                
+                Console.WriteLine("All the tasks in the to do list: \n");
+
+                for (int i = 0; i < taskList.Count; i++)
+                {
+                    Console.WriteLine(taskList[i]);
+                }
+                
+            }
+            // Close the program
+            else if (option == "E" || option == "e")
+            {
+                Console.WriteLine("Closing program");
+            }
+            // Error handling 
+            else
+            {
+                Console.WriteLine("Invalid option, please try again using the valid options shown !");
+            }
+
+
+
+
+
+
+
+
+
 
         }
     }
